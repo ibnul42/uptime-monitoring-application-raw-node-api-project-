d009f4ad73e11cb3464a19fa979c6f69a021187c
ibnul42 <ibnulashir42@gmail.com>
2021-08-19 17:03:45 +0600

    environment variable used

--- index.js
+++ index.js
@@ -8,20 +8,21 @@ Date: 18/08/2021
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
-app.config = {
-    port: 3000,
-};
+// app.config = {
+//     port: 3000,
+// };
 
 // create Server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`Listening to port ${environment.port}`);
     });
 };
 
