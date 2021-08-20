cd170478cdfc29c52223865ba555d410bbf07908
ibnul42 <ibnulashir42@gmail.com>
2021-08-20 12:19:48 +0600

    CRUD operation completed

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ Date: 18/08/2021
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
+
+// testing file system
+// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
+//     console.log(`error was`, err);
+// });
+
+// data.read('test', 'newFile', (err, data) => {
+//     console.log(err, data);
+// });
+
+// data.update('test', 'newFile', { name: 'Australia', language: 'English' }, (err) => {
+//     console.log(`error was`, err);
+// });
+
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
+});
 
 // configuration
 // app.config = {
